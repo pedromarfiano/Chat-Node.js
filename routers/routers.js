6cdb54457bc40c2f7c639da0f192cb459882a8df
Marfian609 <pedromarfiano@gmail.com>
2023-02-22 10:13:42 -0300

    integrando o handlebars

--- routers/routers.js
+++ routers/routers.js
@@ -7,32 +7,59 @@ var path = require('path');
 
 router.use(bodyParser.urlencoded({extended:true}));
 
+// CONFIGURANDO ENDINE HANDLEBARS
 
 // ROTAS DO APP
 router.get('/app/' , (req , res)=>{
     if(req.session.logado){
-        res.sendFile(path.join(__dirname, '../public', 'views/app.html'))
+        const admin = req.session.admin;
+        // puxa o 1 valor de admin que é um array de objetos
+        const row = admin[0]
+        // mostra no termina o valor do id
+        res.render('app', {admin: row})
+
+        // res.sendFile(path.join(__dirname, '../public', 'views/app.html'))
+
     } else{
         res.redirect('/');
     }
 })
 router.get('/app/minha_conta', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public', '/views/conta.html'))
+    if(req.session.logado){
+        const admin = req.session.admin;
+        // puxa o 1 valor de admin que é um array de objetos
+        const row = admin[0]
+        // mostra no termina o valor do id
+        res.render('conta', {admin: row})
+
+        // res.sendFile(path.join(__dirname, '../public', 'views/app.html'))
+
+    } else{
+        res.redirect('/');
+    }
+
+    // res.sendFile(path.join(__dirname+'./views/conta.html'))
 })
 
 
 // ROTAS DO BANCO DE DADOS
 
-router.post('/cadastar', (req,res) => {
+router.post('/cadastrar', (req,res) => {
     let img = "https://api.dicebear.com/5.x/initials/svg?seed="+req.body.User;
     db.query("INSERT INTO tbusers(users_name, users_email, users_pass, users_img) VALUES(?, ?, ?, ?)",
-    [req.body.User, req.body.Email, req.body.Pass, img], (err, result) => {
+    [req.body.User, req.body.Email, req.body.Pass, img], async (err, result) => {
 
         if(err) throw err;
-        console.log(JSON.stringify(result));
+
+        db.query("SELECT * FROM tbusers WHERE users_email = ? and users_pass = ? LIMIT 1;",
+        [req.body.Email, req.body.Pass], async (err, result) => {
+
+        if(err) throw err;
+        req.session.admin = await result;
+        req.session.logado = 'logado';
+        await res.redirect('/app/')
     })
-    // db.inserir('ana', 'ana@gmail.com', '123');
-    res.redirect('/app/')
+    })
 })
 
 
@@ -43,29 +70,45 @@ router.post('/logar', (req, res) => {
 
         if(err) throw err;
         req.session.admin = await result;
-        // res.send(req.session.admin);
         req.session.logado = 'logado';
         await res.redirect('/app/')
     })
 })
 
 
-router.put('/alterar/:id', (req, res) => {
-    // db.alterar('ana', 'ana@gmail.com', '123', req.params.id);
-    res.redirect('/app/minha_conta')
+router.post('/alterar/:id', (req, res) => {
+    db.query("UPDATE tbusers SET users_name = ?, users_email = ?, users_pass = ? WHERE users_id = ? LIMIT 1;",
+    [req.body.User, req.body.Email, req.body.Pass, req.params.id], async (err, result) => {
+
+        if(err) throw err;
+
+        db.query("SELECT * FROM tbusers WHERE users_email = ? and users_pass = ? LIMIT 1;",
+        [req.body.Email, req.body.Pass], async (err, result) => {
+
+            if(err) throw err;
+            req.session.admin = await result;
+            await res.redirect('/app/minha_conta')
+        })
+    })
 })
 
 
-router.delete('/deletar/:id', (req, res) => {
-    // db.deletar(req.params.id)
-    res.redirect('/')
+router.post('/deletar/:id', (req, res) => {
+    db.query("DELETE FROM tbusers WHERE users_id = ?",
+    [req.params.id], (err, result) => {
+        if(err) throw err;
+
+        req.session.destroy(null);
+        // res.clearCookie(this.cookie, { path: '/' });
+        res.redirect('/');
+    })
 })
 
 
 router.post('/sair', (req, res) => {
     // If the user is loggedin
-    if (req.session.conectado) {
-        req.session.conectado = false;
+    if (req.session.logado) {
+        req.session.destroy(null);
         res.redirect('/login');
     }else{
         // Not logged in
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 // CARREGANDO MODULOS
 const app = require('express')();
 const session = require('express-session');
+const { engine } = require('express-handlebars')
 
 // const bodyParser = require('body-parser');
 
@@ -17,8 +18,9 @@ app.use(session({
 
 // ENGINE HTML
 
-// app.engine('html', require('ejs').renderFile);
-// app.set('view engine', 'html');
+app.engine('handlebars', engine());
+app.set('view engine', 'handlebars');
+// app.set('views', './public/views');
 // use.set('views', path.join(__dirname, '/public/views'));
 
 
@@ -37,19 +39,23 @@ router.get('/login', (req, res) => {
     if(req.session.logado)
         res.redirect('/app/')
     
-    res.sendFile(__dirname+'/public/views/login.html')
+    res.render('login')
+    // res.sendFile(__dirname+'/public/views/login.html')
 })
 router.get('/cadastro', (req, res) => {
     if(req.session.logado)
         res.redirect('/app/')
 
-    res.sendFile(__dirname+'/public/views/cadastro.html')
+    res.render('cadastro')
+    // res.sendFile(__dirname+'/public/views/cadastro.html')
 })
 router.get('*' , (req , res)=>{
+
+    //   MUDAR DEPOIS PARA UM ARQUIVO HTML 404
     res.send('ERRO 404')
 })
 
 // OPEN SERVER HTTP
-http.listen(8081, () =>{
+http.listen(8081,'10.0.0.104', () =>{
     console.log('Server connect!')
 });
