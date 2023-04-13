c09ddc1df885eef79597b825233e7b59179c78e8
pedromarfiano <pedromarfiano@gmail.com>
2023-04-13 16:25:55 -0300

    concertando msg

--- messages/messages.js
+++ messages/messages.js
@@ -1,40 +1,101 @@
-const { randomInt, randomUUID } = require('crypto');
 const express = require('express');
 const { Server } = require('socket.io');
 const { router, db } = require('../routers/routers.js');
-
 const app = express();
 const http = require('http').createServer(app);
 const io = new Server(http);
 
-function atualizar() {
-    // db.query("SELECT * FROM tbusers WHERE users_id != ?", [], (err, result) => {
-    //     if(err) throw err;
-        
-    // });
-    
+let clientSocketIds = [];
+let connectedUsers= [];
+
+var userID;
+
+function getID(id) {
+    userID = id
+    console.log(userID);
+    io.on('connection', (socket) => {
+        socket.emit('dados', 
+            emitID = userID,
+            id_socket = socket.id
+        )
+        // socket.emit('uploadList', 
+        //     {}
+        // )
+    })
 }
-let socketUsersID = [];
-//let UsersID = [];
+
+const getSocketByUserId = (userId) =>{
+    let socket = '';
+    for(let i = 0; i<clientSocketIds.length; i++) {
+        if(clientSocketIds[i].userId == userId) {
+            socket = clientSocketIds[i].socket;
+            break;
+        }
+    }
+    return socket;
+}
+
 //SERVIDOR WS
 io.on("connection", (socket) => {
 
-    // AO CONECTAR
-    console.log(`novo socket ${socket.id}`);
-    // MANDA PRO FRONTEND QUE UM SOCKET FOI CONECTADO
-    socket.broadcast.emit('socketEmit', `socket ${socket.id} conectado`);
-    // AO SAIR
+
     socket.on('disconnect', () => {
+        console.log(`Disconnected ${socket.id}`);
         socket.emit('socketEmit', `socket desconectado ${socket.id}`)
-        console.log(`socket desconectado ${socket.id}`);
-    })
-    socket.on('msg', (msg, id) =>{
-        console.log(`email: ${id}, mensagem: ${msg}`)
+        //console.log(`socket desconectado ${socket.id}`);
 
-        socket.emit('msgServer', msg, id);
-        socket.broadcast.emit('msgServer', msg, id);
-
+        connectedUsers = connectedUsers.filter(item => item.socketId != socket.id);
+        io.emit('updateUserList', connectedUsers)
     })
+
+    // AO CONECTAR
+    console.log(`Connected ${socket.id}`);
+    socket.broadcast.emit('socketEmit', `socket ${socket.id} conectado`);
+    
+
+    // MSG
+
+    socket.on('loggedin', function(user) {
+        db.query("SELECT * FROM tbusers WHERE users_id = ?", [user.user], (err, result) => {
+            if (err) throw err;
+
+            socket.emit('UploadListOnline', result[0], user.socket);
+        })
+        // clientSocketIds.push({socket: socket, userId:  user.user_id});
+        // connectedUsers = connectedUsers.filter(item => item.user_id != user.user_id);
+        // connectedUsers.push({...user, socketId: socket.id})
+        // io.emit('updateUserList', connectedUsers)
+    });
+
+    socket.on('GetMsg', (destinatario, remetente) => {
+        db.query("SELECT * FROM tbmessages WHERE msg_remetente_id = ? AND msg_destinatario_id = ? OR msg_remetente_id = ? AND msg_destinatario_id = ? ORDER BY msg_id", [remetente, destinatario, remetente, destinatario], (err, result) => {
+            if (err) throw err;
+
+            socket.emit('UploadListMessage', result);
+        })
+    })
+
+    socket.on('create', function(data) {
+        console.log("create room")
+        socket.join(data.room);
+        let withSocket = getSocketByUserId(data.withUserId);
+        socket.broadcast.to(withSocket.id).emit("invite",{room:data})
+    });
+    socket.on('joinRoom', function(data) {
+        socket.join(data.room.room);
+    });
+
+    socket.on('message', function(data) {
+        socket.broadcast.to(data.room).emit('message', data);
+    })
+
+    // socket.on('msg', (msg, id) =>{
+    //     console.log(`email: ${id}, mensagem: ${msg}`)
+
+    //     socket.emit('msgServer', msg, id);
+    //     socket.broadcast.emit('msgServer', msg, id);
+
+    // })
 
 
 });
@@ -45,26 +106,25 @@ router.get('/app/conversa/:id', (req, res) => {
         const admin = req.session.admin;
         const row = admin[0]
 
-        io.on("connection", (socket) => {
+        getID(row.users_id);
 
-            socket.emit('dados', 
-                id = row.users_id,
-                id_socket = socket.userID
-            )
-            socket.on('msg', (msg, id) =>{
+        // io.on("connection", (socket) => {
 
-                //console.log(`email: ${id}, mensagem: ${msg}`)
+        //     socket.emit('dados', 
+        //         id = row.users_id,
+        //         id_socket = socket.userID
+        //     )
+        //     socket.on('msg', (msg, id) =>{
 
-                //console.log(req.params.id)
 
-                db.query("INSERT INTO tbmessages(msg_remetente_id, msg_destinatario_id, msg) VALUES(?, ?, ?)", [row.users_id, req.params.id, msg], (err, result) =>{
-                    if(err) throw err;
+        //         db.query("INSERT INTO tbmessages(msg_remetente_id, msg_destinatario_id, msg) VALUES(?, ?, ?)", [row.users_id, req.params.id, msg], (err, result) =>{
+        //             if(err) throw err;
                     
 
-                })
+        //         })
         
-            })
-        })
+        //     })
+        // })
 
         db.query("SELECT * FROM tbusers WHERE users_id != ?", [row.users_id], (err, result) => {
             if(err) throw err;
